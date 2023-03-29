365b2ed0acbceb7290bf277cc3325d186df6a05c
Marie Jakob <marie.jakob@psychologie.uni-freiburg.de>
2023-03-29 17:44:04 +0200

    some updates

---
+++ tests/testthat/test-fit_mlsdt.R
@@ -0,0 +1,28 @@
+test_that("make_lme_formula() makes a valid lme4 formula", {
+          expect_equal(make_lme_formula(
+            form_mu = mu ~ x_test,
+            form_lambda = lambda ~ x_test,
+            dv = "Y",
+            trial_type_var = "trial_type",
+            random = "ID",
+            within = c("x_test")
+            ),
+            as.formula("Y ~ trial_type + x_test + x_test:trial_type + (trial_type + x_test + x_test:trial_type | ID)",
+                       env = globalenv()))
+  }
+)
+
+test_that("make_lme_formula() makes a valid formula without within predictors", {
+    expect_equal(make_lme_formula(
+      form_mu = mu ~ x_test,
+      form_lambda = lambda ~ x_test,
+      dv = "Y",
+      trial_type_var = "trial_type",
+      random = "ID",
+      between = c("x_test")
+    ),
+    as.formula("Y ~ trial_type + x_test + x_test:trial_type + (trial_type | ID)",
+              env = globalenv()))
+  }
+)
+
